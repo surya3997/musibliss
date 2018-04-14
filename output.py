9a2ec438c24702cbc485a3beb42d4f2745b3ebf8
surya <surya3997@gmail.com>
2018-04-15 03:12:58 +0530

    basic operations done & removed scrap

--- output.py
+++ output.py
@@ -1,23 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'without_queue.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
 from PyQt4 import QtCore, QtGui
-import pymongo
-from pymongo import MongoClient
-import vlc
-
-try:
-    client = MongoClient("mongodb://localhost:27017")
-    db = client.hd15pd38
-except:
-    print("DB connection error")
-
-http_file_path = "http://localhost/songs"
-
-results = []
-result_cursor = db.songs.find()
-
-for i in result_cursor:
-    results.append(i)
-
-number = len(results)
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -36,7 +25,7 @@ except AttributeError:
 class Ui_widget(object):
     def setupUi(self, widget):
         widget.setObjectName(_fromUtf8("widget"))
-        widget.resize(717, 475)
+        widget.resize(705, 475)
         self.gridLayout = QtGui.QGridLayout(widget)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.horizontalLayout = QtGui.QHBoxLayout()
@@ -74,44 +63,36 @@ class Ui_widget(object):
         self.gridLayout_3 = QtGui.QGridLayout(self.tab)
         self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
         self.treeWidget = QtGui.QTreeWidget(self.tab)
-        self.treeWidget.setGeometry(QtCore.QRect(0, 0, 691, 311))
         self.treeWidget.setSizeIncrement(QtCore.QSize(0, 0))
         self.treeWidget.setObjectName(_fromUtf8("treeWidget"))
-        
-
-        for i in range(number):
-            item_0 = QtGui.QTreeWidgetItem(self.treeWidget)
-
+        self.treeWidget.headerItem().setText(0, _fromUtf8("song"))
+        item_0 = QtGui.QTreeWidgetItem(self.treeWidget)
+        item_0 = QtGui.QTreeWidgetItem(self.treeWidget)
+        item_0 = QtGui.QTreeWidgetItem(self.treeWidget)
         self.gridLayout_3.addWidget(self.treeWidget, 0, 0, 1, 1)
-        
-
         self.tabWidget.addTab(self.tab, _fromUtf8(""))
         self.tab_3 = QtGui.QWidget()
         self.tab_3.setObjectName(_fromUtf8("tab_3"))
         self.gridLayout_2 = QtGui.QGridLayout(self.tab_3)
         self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
-        self.treeWidget_3 = QtGui.QTreeWidget(self.tab_3)
-        self.treeWidget_3.setGeometry(QtCore.QRect(0, 40, 691, 281))
-        self.treeWidget_3.setSizeIncrement(QtCore.QSize(0, 0))
-        self.treeWidget_3.setObjectName(_fromUtf8("treeWidget_3"))
-        item_0 = QtGui.QTreeWidgetItem(self.treeWidget_3)
-        item_0 = QtGui.QTreeWidgetItem(self.treeWidget_3)
-        item_0 = QtGui.QTreeWidgetItem(self.treeWidget_3)
         self.comboBox_3 = QtGui.QComboBox(self.tab_3)
-        self.comboBox_3.setGeometry(QtCore.QRect(10, 10, 91, 27))
         self.comboBox_3.setObjectName(_fromUtf8("comboBox_3"))
         self.comboBox_3.addItem(_fromUtf8(""))
         self.comboBox_3.addItem(_fromUtf8(""))
         self.comboBox_3.addItem(_fromUtf8(""))
         self.gridLayout_2.addWidget(self.comboBox_3, 0, 0, 1, 1)
         self.lineEdit = QtGui.QLineEdit(self.tab_3)
-        self.lineEdit.setGeometry(QtCore.QRect(120, 10, 441, 27))
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.gridLayout_2.addWidget(self.lineEdit, 0, 1, 1, 1)
         self.pushButton = QtGui.QPushButton(self.tab_3)
-        self.pushButton.setGeometry(QtCore.QRect(570, 10, 99, 27))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.gridLayout_2.addWidget(self.pushButton, 0, 2, 1, 1)
+        self.treeWidget_3 = QtGui.QTreeWidget(self.tab_3)
+        self.treeWidget_3.setSizeIncrement(QtCore.QSize(0, 0))
+        self.treeWidget_3.setObjectName(_fromUtf8("treeWidget_3"))
+        item_0 = QtGui.QTreeWidgetItem(self.treeWidget_3)
+        item_0 = QtGui.QTreeWidgetItem(self.treeWidget_3)
+        item_0 = QtGui.QTreeWidgetItem(self.treeWidget_3)
         self.gridLayout_2.addWidget(self.treeWidget_3, 1, 0, 1, 3)
         self.tabWidget.addTab(self.tab_3, _fromUtf8(""))
         self.gridLayout.addWidget(self.tabWidget, 1, 0, 1, 4)
@@ -143,143 +124,34 @@ class Ui_widget(object):
         self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(widget)
 
-    def pressedNextButton(self):
-        if self.current_play_index < number - 1:
-            self.current_play_index += 1
-            play_path = results[self.current_play_index]["song_path"].split("songs")[1]
-            self.play_file = http_file_path + play_path
-            self.play_file = "%20".join(self.play_file.split(" "))
-            print(self.play_file)
-
-            if self.first_time_play == 0:
-                self.vlcPlay.stop()
-            self.first_time_play = 0
-
-            self.vlcPlay = vlc.MediaPlayer(self.play_file)
-            self.vlcPlay.play()
-            self.play_status = 1
-
-        ''' for i in range(number):
-            item_0 = QtGui.QTreeWidgetItem(self.treeWidget)
-
-        for i in range(number):
-            self.treeWidget.topLevelItem(i).setText(0, _translate("widget", str(results[i]["song_title"]), None))
-            self.treeWidget.topLevelItem(i).setText(1, _translate("widget", str(results[i]["song_album"]), None))
-            self.treeWidget.topLevelItem(i).setText(2, _translate("widget", str(results[i]["song_artist"]), None))
-            song_len = results[i]["song_length"]
-            song_length = str(int(song_len / 60)) + ":" + str(int(song_len % 60))
-            self.treeWidget.topLevelItem(i).setText(3, _translate("widget", song_length, None))
-        '''
-        print("Next button is pressed!")
-
-    def pressedPlayButton(self):
-        if self.first_time_play == 1 and number > 0:
-            play_path = results[0]["song_path"].split("songs")[1]
-            self.play_file = http_file_path + play_path
-            self.play_file = "%20".join(self.play_file.split(" "))
-            print(self.play_file)
-            self.first_time_play = 0
-
-            self.vlcPlay = vlc.MediaPlayer(self.play_file)
-            self.vlcPlay.play()
-            self.play_status = 1
-        elif self.first_time_play == 0:
-            if self.play_status == 1:
-                self.vlcPlay.pause()
-                self.play_status = 0
-            else:
-                self.vlcPlay.play()
-                self.play_status = 1
-
-
-        print("Play button is pressed!")
-
-    def pressedBackButton(self):
-        if self.current_play_index > 0:
-            self.current_play_index -= 1
-            play_path = results[self.current_play_index]["song_path"].split("songs")[1]
-            self.play_file = http_file_path + play_path
-            self.play_file = "%20".join(self.play_file.split(" "))
-            print(self.play_file)
-
-            if self.first_time_play == 0:
-                self.vlcPlay.stop()
-            self.first_time_play = 0
-
-            self.vlcPlay = vlc.MediaPlayer(self.play_file)
-            self.vlcPlay.play()
-            self.play_status = 1
-
-        print("Back button is pressed!")
-
-    def indexChangeSort(self, item):
-        print(item)
-
-    def indexChangeSearch(self, item):
-        print(item)
-
-    def pressedSearchButton(self):
-        search_query = self.lineEdit.text()
-        print(search_query)
-
-    def sliderChange(self, valueOfSlider):
-        print(valueOfSlider)
-    
-    def dialChange(self, valueOfDial):
-        self.vlcPlay.audio_set_volume(valueOfDial)
-        print(valueOfDial)
-
-    def clickItemAllSongs(self, item):
-        self.horizontalSlider.setTickInterval(1)
-        for index, i in enumerate(results):
-            if i["song_title"] == item.text(0):
-                play_path = i["song_path"].split("songs")[1]
-                self.play_file = http_file_path + play_path
-                self.play_file = "%20".join(self.play_file.split(" "))
-                print(self.play_file)
-                self.current_play_index = index
-
-                if self.first_time_play == 0:
-                    self.vlcPlay.stop()
-                self.first_time_play = 0
-
-                self.vlcPlay = vlc.MediaPlayer(self.play_file)
-                self.vlcPlay.play()
-                self.play_status = 1
-                break
-
-    def clickItemSearch(self, item):
-        print(item.text(0))
-
-
     def retranslateUi(self, widget):
         widget.setWindowTitle(_translate("widget", "MUSIBLISS", None))
         self.pushButton_4.setText(_translate("widget", "⏪", None))
         self.pushButton_3.setText(_translate("widget", "▮▮ ▶", None))
         self.pushButton_2.setText(_translate("widget", "⏩", None))
-        self.treeWidget.headerItem().setText(0, _fromUtf8("song"))
         self.treeWidget.headerItem().setText(1, _translate("widget", "album", None))
         self.treeWidget.headerItem().setText(2, _translate("widget", "artist", None))
         self.treeWidget.headerItem().setText(3, _translate("widget", "length", None))
         __sortingEnabled = self.treeWidget.isSortingEnabled()
         self.treeWidget.setSortingEnabled(False)
-
-        self.first_time_play = 1
-        self.play_status = 0
-        self.current_play_index = 0
-
-        for i in range(number):
-            self.treeWidget.topLevelItem(i).setText(0, _translate("widget", str(results[i]["song_title"]), None))
-            self.treeWidget.topLevelItem(i).setText(1, _translate("widget", str(results[i]["song_album"]), None))
-            self.treeWidget.topLevelItem(i).setText(2, _translate("widget", str(results[i]["song_artist"]), None))
-            song_len = results[i]["song_length"]
-            song_length = str(int(song_len / 60)) + ":" + str(int(song_len % 60))
-            self.treeWidget.topLevelItem(i).setText(3, _translate("widget", song_length, None))
-
-        
-
+        self.treeWidget.topLevelItem(0).setText(0, _translate("widget", "New Item1", None))
+        self.treeWidget.topLevelItem(0).setText(1, _translate("widget", "gfdssd", None))
+        self.treeWidget.topLevelItem(0).setText(2, _translate("widget", "fsdfsd", None))
+        self.treeWidget.topLevelItem(0).setText(3, _translate("widget", "dfsdfd", None))
+        self.treeWidget.topLevelItem(1).setText(0, _translate("widget", "New Item2", None))
+        self.treeWidget.topLevelItem(1).setText(1, _translate("widget", "dfsdf", None))
+        self.treeWidget.topLevelItem(1).setText(2, _translate("widget", "dfsdf", None))
+        self.treeWidget.topLevelItem(1).setText(3, _translate("widget", "fsdfsd", None))
+        self.treeWidget.topLevelItem(2).setText(0, _translate("widget", "nothing", None))
+        self.treeWidget.topLevelItem(2).setText(1, _translate("widget", "dsfsdf", None))
+        self.treeWidget.topLevelItem(2).setText(2, _translate("widget", "sdfsdfsdfa", None))
+        self.treeWidget.topLevelItem(2).setText(3, _translate("widget", "sdasds", None))
         self.treeWidget.setSortingEnabled(__sortingEnabled)
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("widget", "All Songs", None))
+        self.comboBox_3.setItemText(0, _translate("widget", "Title", None))
+        self.comboBox_3.setItemText(1, _translate("widget", "Album", None))
+        self.comboBox_3.setItemText(2, _translate("widget", "Artist", None))
+        self.pushButton.setText(_translate("widget", "Search", None))
         self.treeWidget_3.headerItem().setText(0, _translate("widget", "song", None))
         self.treeWidget_3.headerItem().setText(1, _translate("widget", "album", None))
         self.treeWidget_3.headerItem().setText(2, _translate("widget", "artist", None))
@@ -299,38 +171,10 @@ class Ui_widget(object):
         self.treeWidget_3.topLevelItem(2).setText(2, _translate("widget", "sdfsdfsdfa", None))
         self.treeWidget_3.topLevelItem(2).setText(3, _translate("widget", "sdasds", None))
         self.treeWidget_3.setSortingEnabled(__sortingEnabled)
-        self.comboBox_3.setItemText(0, _translate("widget", "Title", None))
-        self.comboBox_3.setItemText(1, _translate("widget", "Album", None))
-        self.comboBox_3.setItemText(2, _translate("widget", "Artist", None))
-        self.pushButton.setText(_translate("widget", "Search", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("widget", "Search", None))
         self.label_4.setText(_translate("widget", "Sort by", None))
         self.comboBox_2.setItemText(0, _translate("widget", "Title", None))
         self.comboBox_2.setItemText(1, _translate("widget", "Album", None))
         self.comboBox_2.setItemText(2, _translate("widget", "Artist", None))
 
-
-        # custom function calls
-        self.dial.setValue(99)
-        self.pushButton_2.clicked.connect(self.pressedNextButton)
-        self.pushButton_3.clicked.connect(self.pressedPlayButton)
-        self.pushButton_4.clicked.connect(self.pressedBackButton)
-        self.comboBox_2.currentIndexChanged.connect(self.indexChangeSort)
-        self.comboBox_3.currentIndexChanged.connect(self.indexChangeSearch)
-        self.pushButton.clicked.connect(self.pressedSearchButton)
-        self.horizontalSlider.valueChanged[int].connect(self.sliderChange)
-        self.dial.valueChanged.connect(self.dialChange)
-        self.treeWidget.itemClicked.connect(self.clickItemAllSongs)
-        self.treeWidget_3.itemClicked.connect(self.clickItemSearch)
-
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    widget = QtGui.QWidget()
-    ui = Ui_widget()
-    ui.setupUi(widget)
-    widget.show()
-    sys.exit(app.exec_())
-
+import test_rc
